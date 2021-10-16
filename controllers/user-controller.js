5d310e1dd83c5b5ef853abaf5fcf70099741d048
kamm8899 <kamm8899@gmail.com>
2021-10-16 12:18:38 -0700

    added removeFriend

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -53,10 +53,60 @@ const userController = {
       //Remove User by id
       deleteUser({ params }, res) {
         User.findOneAndDelete({ _id: params.id })
+          .then((dbUserData) => {
+            if (!dbUserData) {
+              res.status(404).json({ message: "No user found with this id" });
+              return;
+            }
+            User.updateMany(
+              { _id: { $in: dbUserData.friends } },
+              { $pull: { friends: params.id } }
+            )
+              .then(() => {
+                Thought.deleteMany({ username: dbUserData.username })
+                  .then(() => {
+                    res.json({ message: "deleted user" });
+                  })
+                  .catch((err) => res.status(400).json(err));
+              })
+              .catch((err) => res.status(400).json(err));
+          })
+          .catch((err) => res.status(400).json(err));
+      },
+
+      //POST to add Friend to user's friend list
+      addFriend({ params }, res) {
+        User.findByIdAndUpdate(
+            { _id: params.id},
+            {$addToSet: {friends: params.friendId}},
+            {new: true}
+        )
+          .select("-_v")
           .then(dbUserData => res.json(dbUserData))
-          .catch(err => res.json(err));
-      }
-      User.updateMany(
-          
-      )
+          .catch(err => {
+              console.log(err);
+              res.status(400).json(err);
+          })
+        },
+        //Delete to remove a friend froma user's friend list
+        deleteFriend({ params }, res) {
+            User.findOneAndUpdate(
+                { _id: params.id},
+                {$pull: {friends: params.friendId}},
+                {new: true, runValidators: true}
+            )
+              .select("-_v")
+              .then(dbUserData => {
+                  if(!dbUserData){
+                      res.status(404).json({message: "No friend was found with this Id."});
+                      return;
+                  }
+                  res.json(dbUserData)
+                })
+              .catch(err => {
+                  console.log(err);
+                  res.status(400).json(err);
+              })
+            }
+
     };
